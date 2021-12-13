4255e66d0cb1c39afb74a1d9977768a389fe50ee
Anna Kucherova <Akucherova010@gmail.com>
2021-12-13 18:07:37 +0200

    Add animation and colors for a hover board

---
+++ app.js
@@ -0,0 +1,34 @@
+const board = document.querySelector('#board');
+const colors = ['#e2b0ff' , '#fff2a7', '#ffa9d0', '#94b7e7', '#ff8585'];
+const SQUARES_NUMBER = 500;
+
+for (let i = 0; i < SQUARES_NUMBER; i++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+
+    square.addEventListener("mouseover", () => {
+        setColor(square);
+    })
+
+    square.addEventListener("mouseleave", () => {
+        removeColor(square);
+    })
+
+    board.append(square);
+}
+
+function setColor(element) {
+    const color = getRandomColor();
+    element.style.background = color;
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+}
+
+function removeColor(element) {
+    element.style.background = '#1d1d1d';
+    element.style.boxShadow = `0 0 2px #000`
+}
+
+function getRandomColor() {
+    const index = Math.floor(Math.random() * colors.length);
+    return colors[index];
+}
